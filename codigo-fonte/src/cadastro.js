3661e003007446b124c74da0f0d3ebd7aefa3743
adriansodr3 <adriansodree@gmail.com>
2023-11-12 11:01:59 -0300

    adicionando funcao para cadastrar uma conta

---
+++ codigo-fonte/src/cadastro.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("form");
+
+class Account {
+  #senha;
+
+  constructor(email, nome, senha) {
+    this.email = email;
+    this.nome = nome;
+    this.#senha = senha;
+  }
+}
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  const email = document.querySelector("#email");
+  const name = document.querySelector("#name");
+  const pass = document.querySelector("#pass");
+  const repeatPass = document.querySelector("#repeatPass");
+
+  if (
+    JSON.parse(localStorage.getItem("contas"))?.find(
+      (user) => user.email === email.value
+    )
+  ) {
+    alert("Já existe um cadastro com esse e-mail");
+    email.focus;
+    return;
+  }
+
+  if (pass.value !== repeatPass.value) {
+    alert("As senhas não coincidem");
+    return;
+  }
+
+  const user = new Account(email.value, name.value, pass.value);
+
+  let accounts = JSON.parse(localStorage.getItem("contas")) ?? [];
+  accounts.push(user);
+
+  localStorage.setItem("contas", JSON.stringify(accounts));
+
+  form.reset();
+});
