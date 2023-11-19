8a3440ba8b5b6a9838963b6c38a886ceb21b484b
adriansodr3 <adriansodree@gmail.com>
2023-11-19 10:24:52 -0300

    ajustando publicacao de receita

--- codigo-fonte/src/publicarReceita.js
+++ codigo-fonte/src/publicarReceita.js
@@ -1,10 +1,12 @@
 const addNewBtn = document.querySelector("#addNew");
 const cancelBtn = document.querySelector("#cancelBtn");
+const divPub = document.querySelector(".publicaReceita");
+
+divPub.style.display = "none";
+divPub.style.position = "unset";
 
 addNewBtn.addEventListener("click", () => {
-  const divPub = document.querySelector(".publicaReceita");
-
-  divPub.style.display = "block";
+  divPub.style.display = "flex";
   divPub.style.position = "absolute";
 });
 
