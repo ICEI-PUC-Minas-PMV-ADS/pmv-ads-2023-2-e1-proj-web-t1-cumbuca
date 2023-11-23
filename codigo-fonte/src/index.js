f5d4f3ad734081e7882fe2b702b30f264e258b09
Bernardo Soutelo <143739215+Bolinhx@users.noreply.github.com>
2023-11-23 19:38:40 -0300

    index.js

---
+++ codigo-fonte/src/index.js
@@ -0,0 +1,12 @@
+const categorias = [
+    {name:'carnes'},
+    {name:'aves'},
+    {name:'peixes e Frutos do mar'},
+    {name:'saladas e molhos'},
+    {name:'bebidas'},
+    {name:'doces e sobremesas'},
+    {name:'massas'},
+    {name:'lanches'},
+    {name:'alimentação saudável'},
+    {name:'bolos e tortas'},
+]
